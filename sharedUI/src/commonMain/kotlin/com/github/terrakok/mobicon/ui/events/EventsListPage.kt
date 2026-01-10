9044978363701e9b843787d019470811112c1189
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-01-10 15:27:41 +0100

    Refactor EventsListPage: add scroll state handling, implement Scaffold with top bar, and improve UI alignment

--- sharedUI/src/commonMain/kotlin/com/github/terrakok/mobicon/ui/events/EventsListPage.kt
+++ sharedUI/src/commonMain/kotlin/com/github/terrakok/mobicon/ui/events/EventsListPage.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,6 +25,7 @@ import kotlinx.datetime.format.Padding
 import kotlinx.datetime.format.char
 import mobicon.sharedui.generated.resources.Res
 import mobicon.sharedui.generated.resources.ic_calendar
+import mobicon.sharedui.generated.resources.ic_close
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
@@ -30,26 +33,53 @@ internal fun EventsListPage(
     onEventClick: (String) -> Unit
 ) {
     val vm = metroViewModel<EventsListViewModel>()
-
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surfaceContainer),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp),
-    ) {
-        itemsIndexed(vm.items, key = { _, e -> e.id }) { index, event ->
-            val y = event.startDate.year
-            if (index == 0 || y != vm.items[index - 1].startDate.year) {
-                Text(
-                    text = y.toString(),
-                    style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.4f),
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp)
-                )
+    val scrollState = rememberLazyListState()
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+        topBar = {
+            Surface(
+                shadowElevation = if (scrollState.canScrollBackward) 8.dp else 0.dp,
+                color = MaterialTheme.colorScheme.surfaceContainerLowest
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.CenterStart
+                ) {
+                    Text(
+                        text = "Select conference",
+                        style = MaterialTheme.typography.titleLarge,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.align(Alignment.Center).padding(horizontal = 60.dp, vertical = 20.dp),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
-            EventInfoCard(event, onEventClick)
+        }
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(MaterialTheme.colorScheme.surfaceContainer),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+            state = scrollState
+        ) {
+            itemsIndexed(vm.items, key = { _, e -> e.id }) { index, event ->
+                val y = event.startDate.year
+                if (index == 0 || y != vm.items[index - 1].startDate.year) {
+                    Text(
+                        text = y.toString(),
+                        style = MaterialTheme.typography.titleMedium,
+                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.4f),
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth().padding(bottom = 12.dp)
+                    )
+                }
+                EventInfoCard(event, onEventClick)
+            }
         }
     }
 }
